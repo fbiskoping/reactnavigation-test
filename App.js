1f88bbd572502b6f0d890513933d5fb6051c3693
Felix Biskoping <biskoping.felix@fh-swf.de>
2023-06-24 01:33:22 +0200

    master

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import { Button, View } from 'react-native';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const Screen1 = ({ navigation }) => (
+  <View style={{ flex: 1 }}>
+    <Button onPress={() => navigation.navigate('Screen2')} title="Next" />
+  </View>
+);
+
+const Screen2 = ({ navigation }) => (
+  <View style={{ flex: 1 }}>
+    <Button onPress={() => navigation.navigate('Screen3')} title="Next" />
+  </View>
+);
+
+const Screen3 = ({ navigation }) => (
+  <View style={{ flex: 1 }}>
+    <Button onPress={() => navigation.navigate('Screen4')} title="Next" />
+  </View>
+);
+
+const Screen4 = () => <View style={{ flex: 1 }} />;
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen component={Screen1} name="Screen1" />
+        <Stack.Screen
+          component={Screen2}
+          name="Screen2"
+          options={{
+            headerBackTitleVisible: false,
+            headerBackTitle: 'Custom title in back button menu',
+          }}
+        />
+        <Stack.Screen
+          component={Screen3}
+          name="Screen3"
+          options={{
+            headerBackTitle: 'Small title',
+            headerBackTitleStyle: { fontSize: 8 },
+          }}
+        />
+        <Stack.Screen
+          component={Screen4}
+          name="Screen4"
+          options={{
+            headerBackTitle: 'Custom title',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
